e232c1dbbb75f523940741c7764d06dbd9b95fa2
Aydin <aydin.metel@gmail.com>
2022-05-09 17:22:29 -0400

    Added shuffle and cut card support

--- java/Shoe.java
+++ java/Shoe.java
@@ -1,26 +1,64 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Shoe {
-    
-    public int numDecks = 1;
-    public ArrayList<Integer> shoe = new ArrayList<Integer>();
-    public int cut_card_index = 0;
 
-    Shoe(){}
+    private int numDecks = 1;
 
-    Shoe(int numDecks){
+    private ArrayList<Integer> shoe = new ArrayList<Integer>();
+    private int cutCardIndex = 0;
+
+    Shoe(int numDecks) {
         this.numDecks = numDecks;
-        this.shoe = createShoe(numDecks);
+        createShoe();
     }
 
-    public static void createDeck(){
-        
+    private ArrayList<Integer> createDeck() {
+        ArrayList<Integer> deck = new ArrayList<>();
+
+        for (int i = 1; i <= 13; i++) {
+            for (int j = 0; j < 4; j++) {
+                deck.add(i);
+            }
+        }
+
+        return deck;
+    }
+
+    public void createShoe() {
+        ArrayList<Integer> deck = createDeck();
+
+        for (int i = 0; i < numDecks; i++) {
+            this.shoe.addAll(deck);
+        }
+        return;
+    }
+
+    public void shuffle() {
+        Collections.shuffle(shoe);
+        placeCutCard();
+
+        return;
+    }
+
+    private void placeCutCard() {
+
+        int lowerBound = shoe.size() / 6;
+        int upperBound = shoe.size() / 2;
+
+        cutCardIndex = ThreadLocalRandom.current().nextInt(lowerBound, upperBound + 1);
+
+        return;
     }
 
 
-
-
-
-
-
+    
+    public void debugInfo() {
+        shuffle();
+        System.out.println(shoe);
+        System.out.println("CC is: " + cutCardIndex);
+        System.out.println("size is: " + shoe.size());
+        return;
+    }
 }
---
+++ java/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String args[]) {
+        Shoe s1 = new Shoe(6);
+        s1.debugInfo();
+
+    }
+
+}

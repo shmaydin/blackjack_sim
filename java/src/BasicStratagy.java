4b5fbe488c74c43fa799f90db16f2ca72de9ca92
Aydin <aydin.metel@gmail.com>
2022-05-11 16:43:26 -0400

    reorganize folders

---
+++ java/src/BasicStratagy.java
@@ -0,0 +1,11 @@
+package src;
+import java.util.HashMap;
+
+public class BasicStratagy {
+    //map  for standard player hand (no aces and not a split)
+    private static final HashMap<Integer,String[]> PLAYER_TOTAL = new HashMap<>();
+    private static final HashMap<Integer,String[]> PLAYER_ACE = new HashMap<>();
+    private static final HashMap<Integer,String[]> PLAYER_PAIR = new HashMap<>();
+    
+
+}
---
+++ java/src/test.java
@@ -0,0 +1,11 @@
+package src;
+public class test {
+    public static void main(String args[]) {
+        Shoe s1 = new Shoe(6);
+        s1.debugInfo();
+        //Hand h1 = new Hand();
+        //h1.debugInfo(s1);
+
+    }
+
+}

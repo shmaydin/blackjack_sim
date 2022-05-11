297536d6149809a02d2c8d05026395dd3ddd4ece
Aydin <aydin.metel@gmail.com>
2022-05-11 13:53:16 -0400

    created basic stratagy class

---
+++ java/BasicStratagy.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class BasicStratagy {
+    //map  for standard player hand (no aces and not a split)
+    private static final HashMap<Integer,String[]> PLAYER_TOTAL = new HashMap<>();
+    private static final HashMap<Integer,String[]> PLAYER_ACE = new HashMap<>();
+    private static final HashMap<Integer,String[]> PLAYER_PAIR = new HashMap<>();
+    
+
+}

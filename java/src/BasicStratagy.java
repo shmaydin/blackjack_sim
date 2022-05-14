c7eb67b575621b7f8e56f6cb14a25c5d407ff7d0
Aydin <aydin.metel@gmail.com>
2022-05-14 16:42:13 -0400

    Added player decision logic

--- java/src/BasicStratagy.java
+++ java/src/BasicStratagy.java
@@ -1,11 +1,113 @@
 package src;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
+//TODO: MUST fix table values. find correct numbers from a mathmatician or something
+// Ive been finding inconsistant data on optimal stratagy
+
 public class BasicStratagy {
-    //map  for standard player hand (no aces and not a split)
-    private static final HashMap<Integer,String[]> PLAYER_TOTAL = new HashMap<>();
-    private static final HashMap<Integer,String[]> PLAYER_ACE = new HashMap<>();
-    private static final HashMap<Integer,String[]> PLAYER_PAIR = new HashMap<>();
-    
+    // map for standard player hand (no aces and not a split)
+    /*
+     * To get correct first index: subtract 8, if less than zero take zero, elif
+     * greater than 9 take 9 - maybe see when tbales are updated
+     * (Starts at 8 and goes to 17)
+     */
+    private static final String[][] PLAYER_HARD_TOTAL = {
+            { "-1", "H", "H", "H", "H", "H", "H", "H", "H", "H", "H", "H", "H", "H" }, // 8
+            { "-1", "H", "H", "D", "D", "D", "D", "H", "H", "H", "H", "H", "H", "H" }, // 9
+            { "-1", "H", "D", "D", "D", "D", "D", "D", "D", "D", "H", "H", "H", "H" }, // 10
+            { "-1", "H", "D", "D", "D", "D", "D", "D", "D", "D", "D", "D", "D", "D" }, // 11
+            { "-1", "H", "H", "H", "S", "S", "S", "H", "H", "H", "H", "H", "H", "H" }, // 12
+            { "-1", "H", "S", "S", "S", "S", "S", "H", "H", "H", "H", "H", "H", "H" }, // 13
+            { "-1", "H", "S", "S", "S", "S", "S", "H", "H", "H", "H", "H", "H", "H" }, // 14
+            { "-1", "H", "S", "S", "S", "S", "S", "H", "H", "H", "H", "H", "H", "H" }, // 15
+            { "-1", "H", "S", "S", "S", "S", "S", "H", "H", "H", "H", "H", "H", "H" }, // 16
+            { "-1", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S" } // 17
+    };
+
+    // table starts at 2 and goes to 9: these numbers represent card other than ace
+    private static final String[][] PLAYER_SOFT_TOTAL = {
+            { "-1", "H", "H", "H", "H", "D", "D", "H", "H", "H", "H", "H", "H", "H" }, // 2
+            { "-1", "H", "H", "H", "H", "D", "D", "H", "H", "H", "H", "H", "H", "H" }, // 3
+            { "-1", "H", "H", "H", "D", "D", "D", "H", "H", "H", "H", "H", "H", "H" }, // 4
+            { "-1", "H", "H", "H", "D", "D", "D", "H", "H", "H", "H", "H", "H", "H" }, // 5
+            { "-1", "H", "H", "D", "D", "D", "D", "H", "H", "H", "H", "H", "H", "H" }, // 6
+            { "-1", "H", "S", "D", "D", "D", "D", "S", "S", "H", "H", "H", "H", "H" }, // 7
+            { "-1", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S" }, // 8
+            { "-1", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S" } // 9
+    };
+
+    // starts at 1 and goes to 10
+    private static final String[][] PLAYER_PAIR = {
+            { "-1", "SP", "SP", "SP", "SP", "SP", "SP", "SP", "SP", "SP", "SP", "SP", "SP", "SP" }, // 1
+            { "-1", "H", "SP", "SP", "SP", "SP", "SP", "SP", "H", "H", "H", "H", "H", "H" }, // 2
+            { "-1", "H", "SP", "SP", "SP", "SP", "SP", "SP", "H", "H", "H", "H", "H", "H" }, // 3
+            { "-1", "H", "H", "H", "H", "SP", "SP", "H", "H", "H", "H", "H", "H", "H" }, // 4
+            { "-1", "H", "D", "D", "D", "D", "D", "D", "D", "D", "H", "H", "H", "H" }, // 5
+            { "-1", "H", "SP", "SP", "SP", "SP", "SP", "H", "H", "H", "H", "H", "H", "H" }, // 6
+            { "-1", "H", "SP", "SP", "SP", "SP", "SP", "SP", "H", "H", "H", "H", "H", "H" }, // 7
+            { "-1", "SP", "SP", "SP", "SP", "SP", "SP", "SP", "SP", "SP", "SP", "SP", "SP", "SP" }, // 8
+            { "-1", "S", "SP", "SP", "SP", "SP", "SP", "S", "SP", "SP", "S", "S", "S", "S" }, // 9
+            { "-1", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S" } // 10
+    };
+
+    /*
+     * Input: A processed version of a player hand - the ArrayList returned from
+     * Hand.getBasicStratagyHand(). Should be length 1 or 2. Never larger and never
+     * empty/Null
+     * 
+     * Output: Returns what action the player should take in the form of a string.
+     * Output are "S": Stand, "H": Hit, "SP": Split, "D": Double Down
+     */
+    public static String getPlayerDecision(ArrayList<Integer> playerHand, int dealerUpCard) {
+        int playerTableIndex;
+
+        // use split table
+        if (useSplitTable(playerHand)) {
+            // get row for table
+            playerTableIndex = playerHand.get(0) - 1;
+            // control for index out of bounds (convert J,Q,K to a 10)
+            if (playerTableIndex > 9) {
+                playerTableIndex = 9;
+            }
+            return PLAYER_PAIR[playerTableIndex][dealerUpCard];
+
+        } else if (useSoftTotalTable(playerHand)) {
+            // get row for table
+            playerTableIndex = playerHand.get(1) - 2;
+            // NOTE: playerHand[1] should never be more than 9
+            return PLAYER_SOFT_TOTAL[playerTableIndex][dealerUpCard];
+
+        } else {
+            if (playerHand.get(0) < 8) {
+                return "H";
+            }
+            if (playerHand.get(0) > 17) {
+                return "S";
+            }
+            playerTableIndex = playerHand.get(0) - 8;
+
+            return PLAYER_HARD_TOTAL[playerTableIndex][dealerUpCard];
+        }
+
+    }
+
+    // given processed player hand, check if we should use the split table
+    private static boolean useSplitTable(ArrayList<Integer> h) {
+        if (h.size() == 2 && h.get(0) == h.get(1)) {
+            return true;
+        }
+        return false;
+    }
+
+    // given processed player hand, check if we should use the soft total table (has
+    // ace)
+    private static boolean useSoftTotalTable(ArrayList<Integer> h) {
+        if (h.size() == 2 && h.get(0) == 1) {
+            return true;
+        }
+        return false;
+    }
 
 }
